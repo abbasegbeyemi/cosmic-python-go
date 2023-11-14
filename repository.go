202a844984a446654297a0e8ff6baf1c79be5e68
Abbas Egbeyemi <abbasegbeyemi@gmail.com>
2023-11-14 09:38:33 +0000

    - Get batch with allocations many to many

--- repository.go
+++ repository.go
@@ -4,10 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 
+	mapset "github.com/deckarep/golang-set/v2"
 	_ "github.com/mattn/go-sqlite3"
 )
-
-const dbFile string = "orders.sqlite"
 
 type SQLRepository struct {
 	db *sql.DB
@@ -19,10 +18,49 @@ func NewSQLRepository() SQLRepository {
 
 const insertBatchRow string = `INSERT INTO batches VALUES(?,?,?,?)`
 
-func (s *SQLRepository) Add(batch Batch) error {
+func (s *SQLRepository) AddBatch(batch Batch) error {
 	if _, err := s.db.Exec(insertBatchRow, batch.reference, batch.sku, batch.quantity, batch.ETA); err != nil {
 		return fmt.Errorf("could not add persist batch to db %w", err)
 	}
 
 	return nil
+}
+
+func (s *SQLRepository) GetBatch(reference Reference) (Batch, error) {
+	batch := Batch{
+		allocations: mapset.NewSet[OrderLine](),
+	}
+
+	row := s.db.QueryRow(`SELECT * FROM "batches" WHERE reference=?`, reference)
+	if err := row.Scan(&batch.reference, &batch.sku, &batch.quantity, &batch.ETA); err != nil {
+		return batch, fmt.Errorf("could not find the requested batch %w", err)
+	}
+
+	// Get the allocations
+	allocationsRows, err := s.db.Query(`SELECT * FROM batches_order_lines WHERE batch_id=?`, reference)
+
+	if err != nil {
+		return batch, fmt.Errorf("could not get allocations for batch: %w", err)
+	}
+	defer allocationsRows.Close()
+
+	for allocationsRows.Next() {
+		var orderID string
+		var batchID string
+		if err := allocationsRows.Scan(&batchID, &orderID); err != nil {
+			return batch, fmt.Errorf("could not scan the order id: %w", err)
+		}
+
+		orderLine := OrderLine{}
+		if err := s.db.QueryRow(`SELECT * FROM order_lines WHERE order_id=?`, orderID).Scan(&orderLine.OrderID, &orderLine.Sku, &orderLine.Quantity); err != nil {
+			return batch, fmt.Errorf("could not scan the order line with id %q: %w", orderID, err)
+		}
+		batch.Allocate(orderLine)
+	}
+
+	if err := allocationsRows.Err(); err != nil {
+		return batch, fmt.Errorf("an error occured while iterating over allocations: %w", err)
+	}
+
+	return batch, nil
 }
--- repository_test.go
+++ repository_test.go
@@ -3,6 +3,7 @@ package cosmicpythongo
 import (
 	"database/sql"
 	"testing"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
@@ -17,14 +18,42 @@ const createBatchesTable string = `
 	);
 `
 
+const createOrderLinesTable string = `
+	CREATE TABLE IF NOT EXISTS order_lines (
+	order_id STRING NOT NULL,
+	sku STRING NOT NULL,
+	quantity INTEGER NOT NULL
+	);
+`
+
+const createBatchesOrderLinesTable string = `
+    CREATE TABLE IF NOT EXISTS batches_order_lines (
+    batch_id STRING NOT NULL,
+    order_id STRING NOT NULL,
+	FOREIGN KEY(batch_id) REFERENCES batches(reference)
+    FOREIGN KEY(order_id) REFERENCES order_lines(order_id)
+	PRIMARY KEY(batch_id, order_id)
+    );
+`
+
 const truncateBatchesTable string = `
 	DELETE FROM batches;
+	DELETE FROM order_lines;
+	DELETE FROM batches_order_lines;
 `
+
+const testDBFile string = "orders_test.sqlite"
 
 func createTables(t *testing.T, db *sql.DB) {
 	t.Helper()
 	if _, err := db.Exec(createBatchesTable); err != nil {
 		t.Fatalf("could not create batches table %s", err)
+	}
+	if _, err := db.Exec(createOrderLinesTable); err != nil {
+		t.Fatalf("could not create order_lines table %s", err)
+	}
+	if _, err := db.Exec(createBatchesOrderLinesTable); err != nil {
+		t.Fatalf("could not create batches_order_lines table %s", err)
 	}
 }
 
@@ -35,8 +64,32 @@ func truncateTables(t *testing.T, db *sql.DB) {
 	}
 }
 
-func TestSQLRepository_Add(t *testing.T) {
-	db, err := sql.Open("sqlite3", dbFile)
+const defaultSku = Sku("LARGE-TABLE")
+const defaultBatchRef = Reference("batch-003")
+const defaultOrderID = Reference("order-004")
+
+func insertBatch(t *testing.T, db *sql.DB) {
+	t.Helper()
+	if _, err := db.Exec("INSERT INTO batches VALUES (?,?,?,?)", defaultBatchRef, defaultSku, 50, time.Now().AddDate(1, 5, 0)); err != nil {
+		t.Fatalf("could not seed the db with batches: %s", err)
+	}
+}
+func insertOrderLine(t *testing.T, db *sql.DB) {
+	t.Helper()
+	if _, err := db.Exec("INSERT INTO order_lines VALUES (?,?,?)", defaultOrderID, defaultSku, 5); err != nil {
+		t.Fatalf("could not seed the db with order lines: %s", err)
+	}
+}
+
+func insertAllocation(t *testing.T, db *sql.DB) {
+	t.Helper()
+	if _, err := db.Exec("INSERT INTO batches_order_lines VALUES (?,?)", defaultBatchRef, defaultOrderID); err != nil {
+		t.Fatalf("could not seed the db with allocations: %s", err)
+	}
+}
+
+func TestSQLRepository_AddBatch(t *testing.T) {
+	db, err := sql.Open("sqlite3", testDBFile)
 	assert.Nil(t, err)
 
 	createTables(t, db)
@@ -51,7 +104,7 @@ func TestSQLRepository_Add(t *testing.T) {
 		repo := SQLRepository{
 			db: db,
 		}
-		err = repo.Add(batch)
+		err = repo.AddBatch(batch)
 		assert.Nil(t, err)
 
 		createdBatch := Batch{}
@@ -60,13 +113,55 @@ func TestSQLRepository_Add(t *testing.T) {
 		assert.Nil(t, err)
 
 		assert.EqualExportedValues(t, batch, createdBatch)
+	})
+}
 
-		// session commit
-		// execute query in pure sql to get batches
-		// assert batches received as expected
+func TestSQLRepository_GetBatch(t *testing.T) {
+	db, err := sql.Open("sqlite3", testDBFile)
+	assert.Nil(t, err)
+
+	createTables(t, db)
+	defer truncateTables(t, db)
+	t.Run("can retrieve batch", func(t *testing.T) {
+		repo := SQLRepository{
+			db: db,
+		}
+
+		existingBatch := Batch{
+			reference: "batch-002",
+			sku:       "LARGE-MIRROR",
+			quantity:  23,
+			ETA:       time.Now().AddDate(0, 3, 0),
+		}
+		db.Exec(insertBatchRow, existingBatch.reference, existingBatch.sku, existingBatch.quantity, existingBatch.ETA)
+
+		receivedBatch, err := repo.GetBatch(existingBatch.reference)
+
+		assert.Nil(t, err)
+		assert.EqualExportedValues(t, existingBatch, receivedBatch)
 	})
 
-	t.Run("can retrieve batch", func(t *testing.T) {
+	t.Run("can retrieve batch with allocation", func(t *testing.T) {
+		db, err := sql.Open("sqlite3", testDBFile)
+		assert.Nil(t, err)
 
+		createTables(t, db)
+		defer truncateTables(t, db)
+
+		insertBatch(t, db)
+		insertOrderLine(t, db)
+		insertAllocation(t, db)
+
+		repo := SQLRepository{
+			db: db,
+		}
+
+		receivedBatch, err := repo.GetBatch(defaultBatchRef)
+
+		assert.Nil(t, err)
+		allocations := receivedBatch.allocations.ToSlice()
+		assert.Greater(t, len(allocations), 0)
+		assert.Equal(t, allocations[0].OrderID, defaultOrderID)
+		assert.Equal(t, allocations[0].Sku, defaultSku)
 	})
 }
